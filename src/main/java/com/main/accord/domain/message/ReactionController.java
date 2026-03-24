8e93cf4ebefd2c5ef5dd31a00a63c763cd9ba424
= <=>
2026-03-24 08:25:10 -0300

    Correção de erro rídiculo do ReactionController

--- src/main/java/com/main/accord/domain/message/ReactionController.java
+++ src/main/java/com/main/accord/domain/message/ReactionController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/messages/{messageId}/reactions")
+@RequestMapping("/api/v1/dm/messages/{messageId}/reactions")
 @RequiredArgsConstructor
 public class ReactionController {
 
