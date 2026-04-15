f77433cca1c34f6c6b6e2c50721a3eb0abdf6deb
Gdev3356 <gko.starkiller@gmail.com>
2026-04-15 09:38:27 -0300

    Actuator Teste

--- src/main/java/com/main/accord/config/SecurityConfig.java
+++ src/main/java/com/main/accord/config/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/v1/ab/levels/*").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/ab/levels/*/leaderboard").permitAll()
                         .requestMatchers("/actuator/health").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
